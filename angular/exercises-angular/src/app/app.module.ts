95ca1857dfe91ee297d395fc2094406b84302aeb
Your Name <you@example.com>
2022-09-23 11:17:37 +0700

    SS

--- angular/exercises-angular/src/app/app.module.ts
+++ angular/exercises-angular/src/app/app.module.ts
@@ -4,20 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
-import {FormsModule} from "@angular/forms";
 import { AngularColorPickerAppComponent } from './angular-color-picker-app/angular-color-picker-app.component';
+
+import { ArticleComponent } from './hackernews-app/article/article.component';
+import { LikeComponent } from './hackernews-app/like/like.component';
+import {FormsModule} from "@angular/forms";
+import { NavbarComponent } from './hackernews-app/navbar/navbar.component';
+import { FooterComponent } from './hackernews-app/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
-    AngularColorPickerAppComponent
+    AngularColorPickerAppComponent,
+    ArticleComponent,
+    LikeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
